988636bdcb91c973206250e40ae83f441698de09
Fulvio Leo <fulviokaldas@outlook.com>
2022-10-03 22:25:18 -0300

    Showing the current cursor position
    - Added a label in the screen that shows the current
    cursor position in the Ln, Col format.

--- src/text-viewer-window.c
+++ src/text-viewer-window.c
@@ -30,6 +30,7 @@ struct _TextViewerWindow
   GtkHeaderBar *header_bar;
   GtkTextView *main_text_view;
   GtkButton *open_button;
+  GtkLabel *cursor_pos;
 };
 
 G_DEFINE_FINAL_TYPE (TextViewerWindow, text_viewer_window, ADW_TYPE_APPLICATION_WINDOW)
@@ -43,6 +44,7 @@ text_viewer_window_class_init (TextViewerWindowClass *klass)
   gtk_widget_class_bind_template_child (widget_class, TextViewerWindow, header_bar);
   gtk_widget_class_bind_template_child (widget_class, TextViewerWindow, main_text_view);
   gtk_widget_class_bind_template_child (widget_class, TextViewerWindow, open_button);
+  gtk_widget_class_bind_template_child (widget_class, TextViewerWindow, cursor_pos);
 }
 
 static void
@@ -178,6 +180,29 @@ gtk_native_dialog_show (GTK_NATIVE_DIALOG (native));
 }
 
 static void
+text_viewer_window__update_cursor_position (GtkTextBuffer *buffer,
+                                            GParamSpec *pspec G_GNUC_UNUSED,
+                                            TextViewerWindow *self)
+{
+  int cursor_pos = 0;
+
+  // Retrieve the value of the "cursor position" property
+  g_object_get (buffer, "cursor-position", &cursor_pos, NULL);
+
+  // Construct the text iterator for the position of the cursor
+  GtkTextIter iter;
+  gtk_text_buffer_get_iter_at_offset (buffer, &iter, cursor_pos);
+
+  // Set the new contents of the label
+  g_autofree char *cursor_str =
+    g_strdup_printf ("Ln %d, Col %d",
+                     gtk_text_iter_get_line (&iter) + 1,
+                     gtk_text_iter_get_line_offset (&iter) + 1);
+
+  gtk_label_set_text (self->cursor_pos, cursor_str);
+}
+
+static void
 text_viewer_window_init (TextViewerWindow *self)
 {
   gtk_widget_init_template (GTK_WIDGET (self));
@@ -190,4 +215,10 @@ text_viewer_window_init (TextViewerWindow *self)
                     self);
   g_action_map_add_action (G_ACTION_MAP (self),
                            G_ACTION (open_action));
+
+  GtkTextBuffer *buffer = gtk_text_view_get_buffer (self->main_text_view);
+  g_signal_connect (buffer,
+                    "notify::cursor-position",
+                    G_CALLBACK (text_viewer_window__update_cursor_position),
+                    self);
 }
